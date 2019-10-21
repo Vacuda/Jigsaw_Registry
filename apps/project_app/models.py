a8252c40f64cc9d07fece607b6e5fd24e6f036a1
Vacuda <adamjsikora@gmail.com>
2019-10-21 17:29:34 -0500

    models first migrated

--- apps/project_app/models.py
+++ apps/project_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from datetime import datetime
+from apps.log_and_reg_app.models import User
+from apps.puzzle_app.models import Puzzle
+import re
 
-# Create your models here.
+class Helper(models.Model):
+    name                = models.CharField(max_length=30)
+    created_at          = models.DateTimeField(auto_now_add=True)
+    updated_at          = models.DateTimeField(auto_now=True)
+    #projects
+
+class Project(models.Model):
+    name                = models.CharField(max_length=30)
+    helpers             = models.ManyToManyField(Helper, related_name="projects")
+    puzzles             = models.ManyToManyField(Puzzle, related_name="projects")
+    started_at          = models.DateTimeField()
+    finished_at         = models.DateTimeField()
+    finished            = models.BooleanField(default=False)
+    created_at          = models.DateTimeField(auto_now_add=True)
+    updated_at          = models.DateTimeField(auto_now=True)
+    #pictures
+
+class Picture(models.Model):
+    caption             = models.CharField(max_length=100)
+    project             = models.ForeignKey(Project, related_name="pictures")            
+    created_at          = models.DateTimeField(auto_now_add=True)
+    updated_at          = models.DateTimeField(auto_now=True)
+
+
+
+
+
+
+
+
+
+
+
--- apps/puzzle_app/models.py
+++ apps/puzzle_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from apps.log_and_reg_app.models import User
 import re
 
 class Brand(models.Model):
@@ -19,17 +20,19 @@ class Puzzle(models.Model):
     brand               = models.ForeignKey(Brand, related_name="puzzles")
     picture             = models.ImageField()
     desc                = models.TextField(max_length=100)
-    category            = models.ForeignKey(Category, related_name="puzzles")
+    category            = models.ManyToManyField(Category, related_name="puzzles")
 
     pieces_labeled      = models.PositiveSmallIntegerField()
     pieces_actual       = models.PositiveSmallIntegerField()
-    length              = models.DecimalField(..., max_digits=5, decimal_places=2)
-    height              = models.PositiveSmallIntegerField()
+    length              = models.DecimalField(max_digits=4, decimal_places=2)
+    height              = models.DecimalField(max_digits=4, decimal_places=2)
 
+    owned_by            = models.ForeignKey(User, related_name="puzzles")
     missing_pieces      = models.PositiveSmallIntegerField()
-    notes               = models.CharField(max_length=30)
+    notes               = models.TextField(max_length=100)
     owned               = models.BooleanField(default=False)
-    completions         = models.IntegerField(default=False)
+    completions         = models.PositiveSmallIntegerField()
     initial_complete    = models.DateTimeField()
     created_at          = models.DateTimeField(auto_now_add=True)
     updated_at          = models.DateTimeField(auto_now=True)
+    #projects
